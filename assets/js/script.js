c5833ad00b81e11b263fd41dcb64a12e0ce03167
Michael Kendrick <michael@locumsmart.net>
2022-02-19 10:48:21 -0600

    refactor

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,20 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function (event) {
+var createFormHandler = function (event) {
   event.preventDefault();
   var taskNameInput = document.querySelector("input[name='task-name']").value;
   var taskTypeInput = document.querySelector("select[name='task-type']").value;
-  console.log(taskTypeInput);
 
+  var taskDataObj = {
+    name: taskNameInput,
+    type: taskTypeInput,
+  };
+
+  createTaskEl(taskDataObj);
+};
+
+var createTaskEl = function (taskDataObj) {
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
 
@@ -14,14 +22,13 @@ var createTaskHandler = function (event) {
   taskInfoEl.className = "task-info";
   taskInfoEl.innerHTML =
     "<h3 class='task-name'>" +
-    taskNameInput +
+    taskDataObj.name +
     "</h3><span class='task-type'>" +
-    taskTypeInput +
+    taskDataObj.type +
     "</span>";
 
   listItemEl.appendChild(taskInfoEl);
 
   tasksToDoEl.appendChild(listItemEl);
 };
-
-formEl.addEventListener("submit", createTaskHandler);
+formEl.addEventListener("submit", createFormHandler);
