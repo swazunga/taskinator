27184bce4fbb8c9f69f188fa4f7102182f0928f3
Michael Kendrick <michael@locumsmart.net>
2022-02-19 18:16:38 -0600

    add ability to update and deletetasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ var pageContentEl = document.querySelector("#page-content");
 var taskIdCounter = 0;
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
-var tasksInProgress = document.querySelector("#tasks-in-progress");
+var tasksInProgressEl = document.querySelector("#tasks-in-progress");
 var tasksCompletedEl = document.querySelector("#tasks-completed");
 
 var completeEditTask = function (taskName, taskType, taskId) {
